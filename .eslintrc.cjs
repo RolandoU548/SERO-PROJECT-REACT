855f16c13f2cb2c1239b2a664e450874110445f1
Rolando <rolandou548@gmail.com>
2023-09-11 15:21:10 -0400

    tab 4

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,36 +1,36 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  settings: {
-    react: {
-      version: "detect"
+    env: {
+        browser: true,
+        es2021: true
+    },
+    settings: {
+        react: {
+            version: "detect"
+        }
+    },
+    extends: ["standard", "plugin:react/recommended"],
+    overrides: [
+        {
+            env: {
+                node: true
+            },
+            files: [".eslintrc.{js,cjs}"],
+            parserOptions: {
+                sourceType: "script"
+            }
+        }
+    ],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+    },
+    plugins: ["react"],
+    rules: {
+        "no-unused-vars": "warn",
+        quotes: "off",
+        semi: "off",
+        "space-before-function-paren": "off",
+        "import/no-absolute-path": "off",
+        indent: "off"
     }
-  },
-  extends: ["standard", "plugin:react/recommended"],
-  overrides: [
-    {
-      env: {
-        node: true
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script"
-      }
-    }
-  ],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module"
-  },
-  plugins: ["react"],
-  rules: {
-    "no-unused-vars": "warn",
-    quotes: "off",
-    semi: "off",
-    "space-before-function-paren": "off",
-    "import/no-absolute-path": "off",
-    indent: "off"
-  }
 };
