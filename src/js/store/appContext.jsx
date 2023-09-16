8dc06c5879c8c8a85f5d9046f16f07998cdde750
Rolando <rolandou548@gmail.com>
2023-09-15 20:37:54 -0400

    useeffect appcontenxt eliminado

--- src/js/store/appContext.jsx
+++ src/js/store/appContext.jsx
@@ -29,14 +29,6 @@ const injectContext = PassedComponent => {
             }
         }, []);
 
-        // useEffect(() => {
-        //     if (state.store.theme === "dark") {
-        //         document.querySelector("html").classList.add("dark");
-        //     } else {
-        //         document.querySelector("html").classList.remove("dark");
-        //     }
-        // }, [state.store.theme]);
-
         // The initial value for the context is not null anymore, but the current state of this component,
         // the context will now have a getStore, getActions and setStore functions available, because they were declared
         // on the state of this component
