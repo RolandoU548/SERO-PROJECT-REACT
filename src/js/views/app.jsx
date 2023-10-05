84a1314ca1a99d28bb7886f2bf8d1914494ea1b8
Rolando <rolandou548@gmail.com>
2023-10-04 22:07:31 -0400

    app a tailwind

--- src/js/views/app.jsx
+++ src/js/views/app.jsx
@@ -10,81 +10,94 @@ export const App = () => {
     const [isOpen, setIsOpen] = useState(false);
     const navigate = useNavigate();
     return (
-        <div className="app">
+        <div className="font-serif text-gray-200 min-h-screen">
+            <video
+                autoPlay
+                loop
+                muted
+                playsInline
+                className="w-[100%] h-[100%] -z-50 absolute object-cover">
+                <source src="SERO_BG.mp4" type="video/mp4" />
+            </video>
             <header className="header-home flex justify-between items-center z-40 fixed top-0 w-full py-5">
                 <h2 className="text-4xl font-semibold ml-10 lg:ml-32 cursor-pointer">
                     SERØ.
                 </h2>
-                <i
-                    className="fa-solid fa-bars"
-                    id="toggle-menu"
-                    onClick={() => {
-                        setIsOpen(!isOpen);
-                    }}></i>
                 <div
-                    className={isOpen ? "block" : "hidden"}
+                    className="hidden resp:flex justify-center items-center mr-5 w-10 h-10 text-2xl bg-[rgba(255,255,255,0.2)] hover:bg-[rgba(255,255,255,0.3)] rounded-full cursor-pointer transition duration-300"
+                    onClick={() => {
+                        setIsOpen(true);
+                    }}>
+                    <i className="fa-solid fa-bars"></i>
+                </div>
+                <div
+                    className={
+                        "resp:fixed resp:top-0 resp:left-0 resp:w-full resp:h-screen resp:bg-[rgba(0,0,0,0.5)]" +
+                        " " +
+                        (isOpen ? "resp:block" : "hidden")
+                    }
                     id="back__menu"
                     onClick={() => {
-                        setIsOpen(!isOpen);
+                        setIsOpen(false);
                     }}></div>
                 <nav
                     className={
-                        "navbar-home mr-4 lg:mr-16 resp:dark:bg-slate-800" +
-                        (isOpen ? " navbar-home-opened" : "")
+                        "navbar-home mr-4 lg:mr-16 resp:dark:bg-slate-800 resp:bg-white resp:m-0 resp:fixed resp:w-60 resp:h-screen resp:top-0 resp:p-7 resp:z-10" +
+                        " " +
+                        (isOpen ? "resp:right-0" : "resp:-right-60")
                     }>
-                    <h2 className="menu-title text-gray-600 dark:text-gray-100 text-center text-4xl font-semibold hidden">
+                    <h2 className="text-gray-600 dark:text-gray-100 text-center text-4xl font-semibold hidden resp:block">
                         SERØ.
                     </h2>
-                    <ul className="flex items-center navbar__ul">
-                        <li>
+                    <ul className="flex items-center resp:mt-5 resp:flex-col">
+                        <li className="my-2.5">
                             <Link
                                 to="/"
-                                className="active text-lg font-medium ml-6 text-gray-200 resp:dark:text-gray-200 resp:text-gray-600">
+                                className="text-lg font-medium ml-6 text-gray-200 hover:text-cyan-300 transition duration-300 resp:dark:text-gray-200 resp:text-gray-600 resp:m-0">
                                 {t("home")}
                             </Link>
                         </li>
-                        <li>
+                        <li className="my-2.5">
                             <Link
                                 to="/services"
-                                className="text-lg font-medium ml-7 text-gray-200 resp:dark:text-gray-200 resp:text-gray-600">
+                                className="text-lg font-medium ml-7 text-gray-200
+                                hover:text-cyan-300 transition duration-300 resp:dark:text-gray-200 resp:text-gray-600 resp:m-0">
                                 {t("services")}
                             </Link>
                         </li>
-                        <li>
+                        <li className="my-2.5">
                             <Link
                                 to="/contact"
-                                className="text-lg font-medium ml-7 text-gray-200 resp:dark:text-gray-200 resp:text-gray-600">
+                                className="text-lg font-medium ml-7 text-gray-200 hover:text-cyan-300 transition duration-300
+                                resp:dark:text-gray-200 resp:text-gray-600 resp:m-0">
                                 {t("contacts")}
                             </Link>
                         </li>
-                        <li>
+                        <li className="my-2.5">
                             <button
-                                className="login-button w-32 text-lg rounded-full p-2 text-black ml-6 resp:dark:bg-gray-100 hover:transition-all bg-gradient-to-r from-white to-[rgba(255,255,255,0.8)] hover:to-cyan-400 duration-1000 hover:text-gray-800"
+                                className="login-button w-32 text-lg rounded-full p-2 text-black ml-6 resp:dark:bg-gray-100 transition duration-300 bg-gradient-to-r from-white to-[rgba(255,255,255,0.8)] hover:to-cyan-400 hover:text-gray-800 resp:m-0 resp:border resp:border-gray-300"
                                 onClick={() => {
                                     navigate("/login");
                                 }}>
                                 {t("login")}
                             </button>
                         </li>
-                        <li>
+                        <li className="my-2.5">
                             <button
-                                className="signup-button w-32 text-lg rounded-full p-2 text-black ml-4 resp:dark:bg-gray-100 hover:transition-all bg-gradient-to-r from-white to-[rgba(255,255,255,0.8)] hover:to-cyan-400 duration-1000 hover:text-gray-800"
+                                className="signup-button w-32 text-lg rounded-full p-2 text-black ml-4 resp:dark:bg-gray-100 transition duration-1000 bg-gradient-to-r from-white to-[rgba(255,255,255,0.8)] hover:to-cyan-400 hover:text-gray-800 resp:m-0 resp:border resp:border-gray-300"
                                 onClick={() => {
                                     navigate("/signup");
                                 }}>
                                 {t("signup")}
                             </button>
                         </li>
-                        <li>
+                        <li className="my-2.5">
                             <LanguageButton className="ml-3 resp:absolute resp:top-3 resp:right-5" />
                         </li>
                     </ul>
                 </nav>
             </header>
-            <video autoPlay loop muted playsInline className="home-video">
-                <source src="SERO_BG.mp4" type="video/mp4" />
-            </video>
-            <div className="triangle z-20 absolute left-1/2 bottom-1.5 -translate-x-1/2 -translate-y-1/2 rounded-xl"></div>
+            <div className="absolute left-1/2 bottom-1.5 -translate-x-1/2 -translate-y-1/2 rounded-xl border-b-[25px] border-l-[60px] border-r-[60px] border-b-cyan-400 border-l-transparent border-r-transparent"></div>
             <h1 className="w-[80%] text-3xl minimum:text-[2.5rem] tiny:text-6xl sm:text-7xl md:text-8xl font-black z-10 text-center absolute text-white top-[47%] left-1/2 -translate-x-1/2 -translate-y-1/2">
                 {t("title")}
             </h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     darkMode: "class",
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                serif: "sans-serif"
+            }
+        },
         screens: {
             minimum: "320px",
             tiny: "480px",
