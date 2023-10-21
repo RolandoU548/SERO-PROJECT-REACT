92d0f7fbbf205343b597ae4c6ee6c18af45d85dc
Rolando <rolandou548@gmail.com>
2023-10-21 00:21:47 -0400

    Prueba imagen aparece en el deploy

--- src/js/views/robertovargas.jsx
+++ src/js/views/robertovargas.jsx
@@ -17,7 +17,7 @@ export const RobertoVargas = () => {
             </h2>
             <div className="glass rounded-2xl w-9/12 ml-40 mt-20 mb-16 flex justify-between">
                 <div
-                    className="cursor-pointer ease-out duration-300 hover:scale-105 w-80 bg-[url('RobertoVargasCard.png')] bg-center bg-cover rounded-2xl h-[23rem] flex justify-center text-white text-3xl font-semibold border-2 border-white"
+                    className="cursor-pointer ease-out duration-300 hover:scale-105 w-80 bg-[url('../img/RobertoVargasCard.png')] bg-center bg-cover rounded-2xl h-[23rem] flex justify-center text-white text-3xl font-semibold border-2 border-white"
                     onClick={() => {
                         navigate("/RobertoVargas");
                     }}>
