47b27d654a33cb6a8306545e57098f6a96303ed2
bynx <sebastiancastrorajbe@gmail.com>
2023-10-24 21:58:38 -0400

    DatabaseBG modified and ClientsBG video added, ProfileBG added

--- src/js/views/dashboard/clients.jsx
+++ src/js/views/dashboard/clients.jsx
@@ -82,10 +82,14 @@ export const Clients = () => {
 
     return (
         <>
-            <img
-                src="ClientsBG.jpg"
-                className="w-screen h-screen -z-50 fixed object-cover top-0 left-0"
-            />
+            <video
+                autoPlay
+                loop
+                muted
+                playsInline
+                className="w-screen h-screen -z-50 fixed top-0 left-0 object-cover">
+                <source src="ClientsBG.mp4" type="video/mp4" />
+            </video>
             <div className="font-serif text-gray-200 mt-28">
                 <h1 className="w-10/12 text-xl minimum:text-[0.5rem] tiny:text-3xl sm:text-7xl md:text-6xl font-black z-10 text-white m-auto">
                     {t("Clients")}
