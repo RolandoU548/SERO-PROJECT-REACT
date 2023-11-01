4c76bfe424db3e73f3e519528adaa251f8454cef
bynx <sebastiancastrorajbe@gmail.com>
2023-10-31 23:51:05 -0400

    borrado el cursor circular, trae muchos problemas

--- src/js/components/Cursor.jsx
+++ src/js/components/Cursor.jsx
@@ -15,47 +15,4 @@ export const Cursor = () => {
         });
     }
     document.addEventListener("click", clickEffect);
-
-    // Cursor circular
-    const cursor = document.querySelector(".c-cursor__inner");
-
-    document.addEventListener("mousemove", moveCursor);
-
-    function moveCursor(e) {
-        const x = e.clientX;
-        const y = e.clientY;
-
-        cursor.style.left = `${x - 12}px`;
-        cursor.style.top = `${y - 8}px`;
-    }
-
-    const links = Array.from(document.querySelectorAll("a"));
-
-    links.forEach(link => {
-        link.addEventListener("mouseover", (e) => {
-            const x = e.clientX;
-            const y = e.clientY;
-            cursor.classList.add("plus");
-            cursor.style.left.add = `${x - 12}px`;
-            cursor.style.top.add = `${y - 8}px`;
-        });
-        link.addEventListener("mouseleave", (e) => {
-            const x = e.clientX;
-            const y = e.clientY;
-            cursor.classList.remove("plus");
-            cursor.style.left.remove = `${x - 12}px`;
-            cursor.style.top.remove = `${y - 8}px`;
-        });
-    });
-
-    return (
-        <>
-            <div className="js-cursor">
-                <div className="c-cursor__inner c-cursor__inner--circle js-cursor-inner">
-                    <div className="c-cursor__side c-cursor__side--left js-cursor-right"></div>
-                    <div className="c-cursor__side c-cursor__side--right js-cursor-left"></div>
-                </div>
-            </div>
-        </>
-    );
 };
