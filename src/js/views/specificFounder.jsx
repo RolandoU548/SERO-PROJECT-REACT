280e99129bc9807a8fc06a642d8247dc6468e4fa
Rolando <rolandou548@gmail.com>
2023-11-05 16:03:10 -0400

    specificContact responsive

--- src/js/views/specificFounder.jsx
+++ src/js/views/specificFounder.jsx
@@ -66,10 +66,14 @@ export const SpecificFounder = () => {
                 <h2 className="mix-blend-difference text-3xl minimum:text-[2.5rem] tiny:text-6xl sm:text-7xl md:text-8xl font-black z-10 text-center text-white mt-32">
                     {data.name}
                 </h2>
-                <div className="glass rounded-2xl w-9/12 h-[25rem] my-16 m-auto flex justify-between relative">
+                <div className="glass rounded-2xl md:w-9/12 w-10/12 my-16 m-auto flex justify-between relative flex-col lg:flex-row">
+                    <BackArrow
+                        className="absolute right-5 lg:text-black text-white"
+                        to="/founders"
+                    />
                     <div
                         className={
-                            "cursor-pointer transition ease-out duration-300 hover:scale-105 lg:w-96 w-full bg-cover rounded-2xl h-[25rem] border-2 border-white relative" +
+                            "cursor-pointer transition ease-out duration-300 hover:scale-105 lg:w-4/5 w-full bg-cover rounded-2xl h-[30rem] border-2 border-white relative" +
                             " " +
                             (founder === "SebastianCastroRajbe"
                                 ? "bg-[url('https://firebasestorage.googleapis.com/v0/b/ser0-project.appspot.com/o/images%2Fcontact%2FSebastianCastroRajbeCard.jpg?alt=media&token=232c13b9-a942-41cf-bdf8-7ada779fb8b8&_gl=1*13pgz92*_ga*NzgxNTMyNDcyLjE2OTg0NDk1MjI.*_ga_CW55HF8NVT*MTY5ODU1ODYyNS40LjEuMTY5ODU1OTYzNi42MC4wLjA.')] bg-center"
@@ -78,7 +82,7 @@ export const SpecificFounder = () => {
                                 : founder === "RobertoVargas"
                                 ? "bg-[url('https://firebasestorage.googleapis.com/v0/b/ser0-project.appspot.com/o/images%2Fcontact%2FRobertoVargasCard.jpg?alt=media&token=3ffe1fb7-69fd-4476-baa3-61a7673e5225&_gl=1*1y6d1f8*_ga*NzgxNTMyNDcyLjE2OTg0NDk1MjI.*_ga_CW55HF8NVT*MTY5ODU1ODYyNS40LjEuMTY5ODU1OTYxMy4xNi4wLjA.')] bg-left"
                                 : founder === "RolandoUzcategui"
-                                ? "bg-[url('https://firebasestorage.googleapis.com/v0/b/ser0-project.appspot.com/o/images%2Fcontact%2FRolandoUzcateguiCard.jpg?alt=media&token=ea051e60-71ae-406e-87d6-85a62a382d77&_gl=1*7ifq9c*_ga*NzgxNTMyNDcyLjE2OTg0NDk1MjI.*_ga_CW55HF8NVT*MTY5OTA3NjA3Ni43LjEuMTY5OTA3NjEzMi40LjAuMA..')] bg-center"
+                                ? "bg-[url('https://firebasestorage.googleapis.com/v0/b/ser0-project.appspot.com/o/images%2Fcontact%2FRolandoUzcateguiCard.jpg?alt=media&token=ea051e60-71ae-406e-87d6-85a62a382d77&_gl=1*7ifq9c*_ga*NzgxNTMyNDcyLjE2OTg0NDk1MjI.*_ga_CW55HF8NVT*MTY5OTA3NjA3Ni43LjEuMTY5OTA3NjEzMi40LjAuMA..')] bg-top"
                                 : "")
                         }
                         onClick={() => {
@@ -86,7 +90,7 @@ export const SpecificFounder = () => {
                         }}>
                         <div className="bg-black absolute w-full h-full rounded-2xl ease-out duration-300 hover:opacity-30 opacity-50 left-0 top-0"></div>
                     </div>
-                    <div className="md:w-[50rem] w-full p-10 text-lg flex items-center">
+                    <div className="lg:1/5 w-full p-5 sm:p-10 lg:pt-0 sm:text-lg tiny:text-base minimum:text-md text-sm flex items-center">
                         <div>
                             <h2>
                                 {t("aka")} {data.nickname}.
