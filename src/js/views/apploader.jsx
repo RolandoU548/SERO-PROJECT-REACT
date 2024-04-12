4d6263028e797669aa3c8b4106e5eefb91f4c8fc
RolandoU548 <rolandou548@gmail.com>
2024-04-12 02:23:48 -0400

    apploader mejorado

--- src/js/views/apploader.jsx
+++ src/js/views/apploader.jsx
@@ -3,7 +3,7 @@ import "../../css/apploader.css";
 
 export const AppLoader = () => {
     return (
-        <div className="bg-black z-[1000] top-0 flex flex-col justify-center items-center h-screen scale-100 apploader text-cyan-300 text-7xl font-serif font-bold ">
+        <div className="bg-black z-[5000] top-0 flex flex-col justify-center items-center h-screen apploader text-cyan-300 text-7xl font-serif font-bold ">
             SERØ.
         </div>
     );
